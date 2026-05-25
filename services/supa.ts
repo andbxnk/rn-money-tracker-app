26fb7b6920cf21b3c00d295b200c279b0071edf7
andbxnk <tanabut1955@gmail.com>
2026-05-25 19:23:32 +0700

    first commit

--- services/supa.ts
+++ services/supa.ts
@@ -2,7 +2,6 @@ import { createClient } from "@supabase/supabase-js";
 import "react-native-url-polyfill/auto";
 
 const SUPABASE_URL = "https://ohgwhekwwykmeynczqaq.supabase.co";
-// ✨ ใส่ Key ตัวจริงกลับคืนมาให้เรียบร้อยครับ
 const SUPABASE_ANON_KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9oZ3doZWt3d3lrbWV5bmN6cWFxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg0MTEzNzAsImV4cCI6MjA5Mzk4NzM3MH0.5jOuo6PDgWijeorV53hQRgWQyhUH1DX46cWJQI7sMZI";
 
@@ -21,22 +20,19 @@ export const transactionService = {
     return { data, error };
   },
 
-  // 2. ดึงประวัติรายการ (ปรับเพิ่มการรับค่า limit และ page และส่งค่า count กลับไปเพื่อทำ Pagination)
   async getTransactions(limit: number = 15, page: number = 1) {
-    // คำนวณหาจุดเริ่มต้น (from) และจุดสิ้นสุด (to) ของข้อมูลในหน้านั้นๆ
     const from = (page - 1) * limit;
     const to = from + limit - 1;
 
     const { data, error, count } = await supabase
       .from("transactions")
-      .select("*", { count: "exact" }) // ✨ เพิ่มตรงนี้เพื่อให้ Supabase ส่งจำนวนรายการทั้งหมดที่มีกลับมาด้วย
+      .select("*", { count: "exact" }) // ✨
       .order("created_at", { ascending: false })
-      .range(from, to); // ใช้ range ดึงเฉพาะช่วงข้อมูลที่คำนวณได้
+      .range(from, to); //
 
-    return { data, error, count }; // ✨ ส่ง count เพิ่มกลับไปให้หน้าจอเช็กเงื่อนไขปุ่มกดถัดไป
+    return { data, error, count }; // ✨
   },
 
-  // 3. คำนวณสรุปยอดเงิน
   async getSummary() {
     const { data, error } = await supabase
       .from("transactions")
